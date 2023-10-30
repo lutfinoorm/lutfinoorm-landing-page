756ad58c28f59c0e0312f126c5104df191173617
lutfinoor muhamad <lutfinoor.muhamad@gmail.com>
2023-10-30 19:27:53 +0700

    test: linking css and js to html

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Start project:
 - create index.html
 - create style.css
 - create main.js
+- linking css and javascript to html
 
---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById('alert').addEventListener('click', () => {
+    alert("Testing javascript function!")
+})
